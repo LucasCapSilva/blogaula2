0f886ef4c605bf819bfb6214f7ab3bd277736db5
unknown <generation@email.com>
2020-10-13 07:45:50 -0300

    segundo commit

--- blogPessoal-api/src/main/java/generation/org/blogPessoal/controller/PostagemController.java
+++ blogPessoal-api/src/main/java/generation/org/blogPessoal/controller/PostagemController.java
@@ -42,11 +42,12 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(repositoty.findAllByTituloContainingIgnoreCase(titulo));
 	}
-	 
+	
 	@PostMapping
 	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repositoty.save(postagem));
 	}
+	
 	@PostMapping("/all")
 	public ResponseEntity<List<Postagem>> postAll (@RequestBody List<Postagem> postagem){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repositoty.saveAll(postagem));
